97d3c45d7163821766f35a8574f2ae2ff87a42de
Darryl Johnson <dandycodes@gmail.com>
2022-08-30 07:35:04 -0400

    server not running bug needs to be fixed 8/30

---
+++ servers.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('The Server Works Fam!')
+})
