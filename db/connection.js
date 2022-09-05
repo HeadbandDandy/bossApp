caa77eef5f1eb62c71dcf0610f943204158e6f19
Darryl Johnson <dandycodes@gmail.com>
2022-09-05 17:10:34 -0400

    database seeded connection to SQL added 9/5

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,12 @@
 const mysql = require('mysql2')
+
+
+const db = mysql.createConnection({ 
+    host: 'localhost',
+    user: 'dandy',
+    password: 'AllHailKIngDandy1!',
+    database: 'election'
+});
+
+
+module.exports = db;
